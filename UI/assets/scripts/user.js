3083743ec64a28c144a3068a8a0b767638ee4a62
enwankwo <cnwankwom@gmai.com>
2019-06-18 09:14:09 +0100

    [feature #166639137] Implement search specific property type

---
+++ UI/assets/scripts/user.js
@@ -0,0 +1,12 @@
+const searchButton = document.querySelector('#search-type-btn');
+const searchPurpose = document.querySelector('#search-purpose');
+const searchBedroom = document.querySelector('#search-bedroom');
+
+const show = () => {
+    console.log('clicked');
+}
+
+const searchProperty = () => {
+    
+}
+searchButton.addEventListener('click', show);
