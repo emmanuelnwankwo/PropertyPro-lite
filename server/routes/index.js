acc2c5eca72a255f5ddd90a6b0e9f8a17339bdea
enwankwo <cnwankwom@gmai.com>
2019-06-24 12:33:14 +0100

    [feature #166857308]implement API endpoint for delete property

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ import Property from '../controllers/dummyController/propertyController';
 
 const router = express.Router();
 
-const { createProperty, getProperties, getProperty } = Property;
+const {
+  createProperty, getProperties, getProperty, deleteProperty,
+} = Property;
 
 router.get('/', (req, res) => {
   res.send('Welcome to PropertyPro-Lite');
@@ -14,6 +16,7 @@ const propertyUrl = '/api/v1/property';
 router.post(`${propertyUrl}`, createProperty);
 router.get(`${propertyUrl}`, getProperties);
 router.get(`${propertyUrl}/:propertyId`, getProperty);
+router.delete(`${propertyUrl}/:propertyId`, deleteProperty);
 
 /** End Property Routes */
 
