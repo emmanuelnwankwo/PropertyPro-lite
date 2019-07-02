2cc02def820db8b6d9ad18fbfa6ffcacfe36c755
enwankwo <cnwankwom@gmai.com>
2019-07-02 11:09:39 +0100

    feature(API)Fixed authorization and validations

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Property from '../controllers/dummyController/propertyController';
 import User from '../controllers/dummyController/userController';
 import AuthValidator from '../middlewares/authValidator';
+import PropertyValidator from '../middlewares/propertyValidator';
 
 const router = express.Router();
 
@@ -15,33 +16,33 @@ const {
   markProperty,
   typeProperty,
 } = Property;
-const { validateSignUp, userExists, validatePhone } = AuthValidator;
-
+const {
+  validateSignUp,
+  userExists,
+  validatePhone,
+  isAuthenticated,
+  isAgent,
+} = AuthValidator;
+const { validateProperty, validateSingleProperty } = PropertyValidator;
 router.get('/', (req, res) => {
   res.send('Welcome to PropertyPro-Lite');
 });
 
 /** Property Routes */
 const propertyUrl = '/api/v1/property';
-router.post(`${propertyUrl}`, createProperty);
-router.get(`${propertyUrl}`, getProperties);
-router.get(`${propertyUrl}/:propertyId`, getProperty);
-router.get(`${propertyUrl}/search/:propertyType`, typeProperty);
-router.delete(`${propertyUrl}/:propertyId`, deleteProperty);
-router.put(`${propertyUrl}/:propertyId`, updateProperty);
-router.patch(`${propertyUrl}/:propertyId`, markProperty);
+router.post(`${propertyUrl}`, isAuthenticated, isAgent, validateProperty, createProperty);
+router.get(`${propertyUrl}`, isAuthenticated, getProperties);
+router.get(`${propertyUrl}/:propertyId`, isAuthenticated, validateSingleProperty, getProperty);
+router.get(`${propertyUrl}/search/:propertyType`, isAuthenticated, typeProperty);
+router.delete(`${propertyUrl}/:propertyId`, isAuthenticated, isAgent, validateSingleProperty, deleteProperty);
+router.put(`${propertyUrl}/:propertyId`, isAuthenticated, isAgent, validateSingleProperty, updateProperty);
+router.patch(`${propertyUrl}/:propertyId`, isAuthenticated, isAgent, validateSingleProperty, markProperty);
 
 /** End Property Routes */
 
 /**  authBaseUrl Routes */
 const authBaseUrl = '/api/v1/auth';
-router.post(
-  `${authBaseUrl}/signup`,
-  validateSignUp,
-  validatePhone,
-  userExists,
-  createUser,
-);
+router.post(`${authBaseUrl}/signup`, validateSignUp, userExists, validatePhone, createUser);
 router.post(`${authBaseUrl}/login`, loginUser);
 
 export default router;
