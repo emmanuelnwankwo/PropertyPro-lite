e19434bb00733011f201f934117cb217bb62ce13
enwankwo <cnwankwom@gmai.com>
2019-07-07 22:07:24 +0100

    feature(API)Review:implement create property endpoint

---
+++ server/controllers/propertyController.js
@@ -0,0 +1,52 @@
+import Authenticator from '../helper/authenticator';
+import pool from '../config/connection';
+
+const { decodeToken } = Authenticator;
+const header = (req) => {
+  const token = req.headers.authorization.split(' ')[1] || req.headers.authorization;
+  const decoded = decodeToken(token);
+  return decoded.payload;
+};
+let property;
+/**
+ * Defines methods for properties
+ * @class PropertyController
+ */
+class PropertyController {
+
+  /**
+     * Creates a property
+     * @static
+     * @param {object} req - request
+     * @param {object} res - response
+     * @returns
+     * @memberof PropertyController
+     */
+  static async createProperty(req, res) {
+    const owner = header(req).id;
+    const ownerPhone = header(req).phoneNumber;
+    const ownerEmail = header(req).email;
+    const client = await pool.connect();
+    try {
+      const {
+        status, price, state, city, address, type, imageUrl, propertyName, imageUrl2, imageUrl3, description, mapLat, mapLng, purpose,
+      } = req.body;
+      const sqlQuery = `INSERT INTO properties(owner, propertyName, status, type, state, city, address, price, imageUrl, imageUrl2, imageUrl3, ownerEmail, ownerPhone, purpose, description, mapLat, mapLng)
+                    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+                    RETURNING *`;
+      const values = [owner, propertyName, status, type, state, city, address, price, imageUrl, imageUrl2, imageUrl3, ownerEmail, ownerPhone, purpose, description, mapLat, mapLng];
+      property = await client.query({ text: sqlQuery, values });
+      if (property.rows && property.rowCount) {
+        property = property.rows;
+        return res.status(201).json({ status: 'success', data: property });
+      }
+    } catch (err) {
+      return res.status(404).json({ status: 'error', error: 'User ID does not exists in database' });
+    } finally {
+      await client.release();
+    }
+    return null;
+  }
+}
+
+export default PropertyController;
