55e24fe37dad206ab99ff77c10f4c1203e965402
enwankwo <cnwankwom@gmai.com>
2019-07-07 17:47:35 +0100

    feature(API)Review:implement endpoint for login

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -27,6 +27,23 @@ class UserModel {
     }
   }
 
+  static async getUser(colomn, value, res) {
+    const text = `SELECT * FROM users WHERE ${colomn} = $1`;
+    const values = [value];
+    let user;
+    const client = await pool.connect();
+    try {
+      user = await client.query({ text, values });
+      if (user.rows && user.rowCount) {
+        return user.rows[0];
+      }
+      return null;
+    } catch (err) {
+      return res.status(500).json({ status: 'error', error: 'Internal Server error' });
+    } finally {
+      await client.release();
+    }
+  }
 }
 
 export default UserModel;
