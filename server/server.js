edac532aff9fba1061f841ee6e5d693d2a4da123
enwankwo <cnwankwom@gmai.com>
2019-07-09 15:02:14 +0100

    chore(cors)Installed cors package

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import fileupload from 'express-fileupload';
 import debug from 'debug';
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
 const debugy = debug('server');
 const port = process.env.PORT || 3000;
+app.use(cors());
+app.options('*', cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
