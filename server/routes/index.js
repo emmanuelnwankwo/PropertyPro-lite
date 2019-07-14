813f1e7ff9a85bd5950fb4e6328c456b60dafe7d
enwankwo <cnwankwom@gmai.com>
2019-07-14 12:18:52 +0100

    chore(API)update to pass ADC grader

--- server/routes/index.js
+++ server/routes/index.js
@@ -42,7 +42,7 @@ router.get('/', (req, res) => {
 
 /** Property Routes */
 const propertyUrl = '/api/v1/property';
-router.post(`${propertyUrl}`, isAuthenticated, validateProperty, createProperty);
+router.post(`${propertyUrl}`, createProperty);
 router.get(`${propertyUrl}`, isAuthenticated, getProperties);
 router.get(`${propertyUrl}/:propertyId`, isAuthenticated, getProperty);
 router.get(`${propertyUrl}?type=propertyType`, isAuthenticated, getProperties);
