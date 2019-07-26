6c76ed36bf5cc57f6436a2448f6807e6fc4be3e1
enwankwo <cnwankwom@gmai.com>
2019-07-26 09:33:06 +0100

    chore(API) refactor code

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -2,9 +2,6 @@
 import Validator from 'validator';
 import isEmpty from './is-empty';
 
-// Credit: https://stackoverflow.com/questions/16299036/to-check-if-a-string-is-alphanumeric-in-javascript
-const regex = /((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i;
-
 const checkSignup = (data) => {
   const errors = {};
   data.email = !isEmpty(data.email) ? data.email : '';
@@ -18,12 +15,6 @@ const checkSignup = (data) => {
   if (Validator.isEmpty(data.email)) {
     errors.email = 'Email is required';
   }
-  // if (!regex.test(data.password)) {
-  //   errors.password = 'Your Password must contain atleast 2 numbers and letter';
-  // }
-  // if (!Validator.isLength(data.password, { min: 8 })) {
-  //   errors.password = 'Your Password must be up to 8 digits';
-  // }
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password is required';
   }
@@ -58,12 +49,6 @@ const checkLogin = (data) => {
   if (Validator.isEmpty(data.email)) {
     errors.email = 'Email is required';
   }
-  // if (!regex.test(data.password)) {
-  //   errors.password = 'Your Password must contain atleast 2 numbers and letter';
-  // }
-  // if (!Validator.isLength(data.password, { min: 8 })) {
-  //   errors.password = 'Your Password must be up to 8 digits';
-  // }
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password is required';
   }
