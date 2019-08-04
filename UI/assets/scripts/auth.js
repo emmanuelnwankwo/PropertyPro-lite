fae7e44d119a0d65c4a1b7c7816a10dea690b654
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-04 20:38:14 +0100

    chore(UI)Power UI pages with API endpoints

--- UI/assets/scripts/auth.js
+++ UI/assets/scripts/auth.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 const accessToken = localStorage.getItem('token');
-const urlPath = window.location.pathname.split('/')[2];
+const urlPath = window.location.pathname.split('/')[3];
 const tokenUrl = 'https://propertypro-lit.herokuapp.com/api/v1/token';
 const isAuthenticated = (token) => {
   if (!token) window.location = 'login.html';
@@ -17,23 +17,22 @@ const isAuthenticated = (token) => {
       .then(res => res.json())
       .then((res) => {
         const { is_admin, user_type } = res.data;
-      console.log(urlPath);
-//         if (res.status !== 200) {
-//           window.location = 'login.html';
-//         }
-//         if (is_admin) {
-//           if (!(urlPath === 'admin.html')) {
-//             window.location.replace('admin.html');
-//           }
-//         } else if (!is_admin && user_type === 'agent') {
-//           if (!(urlPath === 'agent.html' || urlPath === 'post.html' || urlPath === 'update.html')) {
-//             window.location.replace('agent.html');
-//           }
-//         } else if (user_type === 'user') {
-//           if (!(urlPath === 'user.html' || urlPath === 'property.html')) {
-//             window.location.replace('user.html');
-//           }
-//         }
+        if (res.status !== 200) {
+          window.location = 'login.html';
+        }
+        if (is_admin) {
+          if (!(urlPath === 'admin.html')) {
+            window.location.replace('admin.html');
+          }
+        } else if (!is_admin && user_type === 'agent') {
+          if (!(urlPath === 'agent.html' || urlPath === 'post.html' || urlPath === 'update.html')) {
+            window.location.replace('agent.html');
+          }
+        } else if (user_type === 'user') {
+          if (!(urlPath === 'user.html' || urlPath === 'property.html')) {
+            window.location.replace('user.html');
+          }
+        }
       })
       .catch((err) => {
         console.log(err);
